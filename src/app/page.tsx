39be8618b5436625bb329dcda0b585e50043e9b1
Felipe Mota <felipe.msrb@gmail.com>
2023-06-10 11:08:25 +0100

    Adicionado MUI e mudança no paragrafo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-        Teste
+        Teste2
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
