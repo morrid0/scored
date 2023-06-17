5dd2eb9a407d8bf8d4a013cca1284d6dbf8d7290
Luiz Cavalcanti <luiz@fanvue.com>
2023-06-17 09:14:30 +0100

    Ajustado import

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import styles from "../page.module.css";
-import LoginForm from "@/app/components/login/LoginForm";
+import LoginForm from "../components/login/LoginForm";
 import Navbar from "../components/navbar/Navbar";
 
 export default function Page() {
